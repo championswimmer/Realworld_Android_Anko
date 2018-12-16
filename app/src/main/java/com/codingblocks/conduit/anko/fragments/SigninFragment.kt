6b1be45f2ee899d6dfab446ccb8c02acbfbede4f
Arnav Gupta <arnav@codingblocks.com>
2018-12-17 05:04:25 +0530

    add fragment stubs
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/conduit/anko/fragments/SigninFragment.kt
@@ -0,0 +1,28 @@
+package com.codingblocks.conduit.anko.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.codingblocks.conduit.anko.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SigninFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+
+}
