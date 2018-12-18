4b0fb58a6280c9f5d9881681d4d7846bb9cfe41f
Arnav Gupta <arnav@codingblocks.com>
2018-12-18 06:48:54 +0530

    open fragments from navdrawer clicks
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/codingblocks/conduit/anko/MainActivity.kt
+++ app/src/main/java/com/codingblocks/conduit/anko/MainActivity.kt
@@ -2,15 +2,40 @@ package com.codingblocks.conduit.anko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.core.view.iterator
+import com.codingblocks.conduit.anko.fragments.HomeFragment
+import com.codingblocks.conduit.anko.fragments.SigninFragment
+import com.codingblocks.conduit.anko.fragments.SignupFragment
 import com.codingblocks.conduit.anko.layouts.activities.MainActivityUI
+import com.google.android.material.navigation.NavigationView
 import org.jetbrains.anko.setContentView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var ui: MainActivityUI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val ui = MainActivityUI().also {
+        ui = MainActivityUI().also {
             it.setContentView(this)
+            it.nav.setNavigationItemSelectedListener(this)
         }
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment =  when (item.itemId) {
+            R.id.menu_home -> HomeFragment()
+            R.id.menu_signin -> SigninFragment()
+            R.id.menu_signup -> SignupFragment()
+            else -> HomeFragment()
+        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(ui.container.id, fragment)
+            .commit()
+        item.isChecked = true
+        ui.drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
--- app/src/main/java/com/codingblocks/conduit/anko/layouts/activities/MainActivityUI.kt
+++ app/src/main/java/com/codingblocks/conduit/anko/layouts/activities/MainActivityUI.kt
@@ -3,29 +3,36 @@ package com.codingblocks.conduit.anko.layouts.activities
 import android.content.Context
 import android.view.Gravity
 import android.view.View
+import android.widget.FrameLayout
+import androidx.annotation.IdRes
+import androidx.drawerlayout.widget.DrawerLayout
 import com.codingblocks.conduit.anko.R
 import com.codingblocks.conduit.anko.extensions.importView
+import com.google.android.material.navigation.NavigationView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.navigationView
 import org.jetbrains.anko.support.v4.drawerLayout
 
 class MainActivityUI: AnkoComponent<Context> {
+    lateinit var drawer: DrawerLayout
+    lateinit var nav: NavigationView
+    lateinit var container: FrameLayout
 
     override fun createView(ui: AnkoContext<Context>): View = with(ui) {
-        drawerLayout {
+        drawer = drawerLayout {
             fitsSystemWindows = true
-            frameLayout {
-                textView("Hello World") {
 
-                }.lparams(wrapContent, wrapContent, Gravity.CENTER)
+            container = frameLayout {
+                id = View.generateViewId()
             }.lparams(matchParent, matchParent)
 
-            navigationView {
+            nav = navigationView {
                 fitsSystemWindows = true
                 addHeaderView(importView(NavDrawerHeaderUI()))
                 inflateMenu(R.menu.main_menu_loggedout)
             }.lparams(wrapContent, matchParent, Gravity.START)
         }
+        drawer
     }
 
     class NavDrawerHeaderUI: AnkoComponent<Context> {
